ad4a07ac24c6654c0bf798ec6723c7e09b1d7ebe
Khwajapasha <Khwajapasha141@gamil.com>
2020-09-18 21:38:11 +0530

    added inline stylesheet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 export const Home =()=> {
         return(
-            <div>
+            <div  >
                 
-                <h1 > Welcome To Home </h1>  
-                <p >
+                <h1 style={{color: "red",backgroundColor:"black"}}> Welcome To Home </h1>  
+                <p style={{backgroundColor: "lightblue"}}>
                     Hello, 
                     This page is made up by Mr. Khwaja  and he is very passionate to learn new things.
                 </p>
-                
+
                 
             </div>
         )
