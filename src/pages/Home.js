feda5bc9292a38a5c55d0823f4f33f80794fe736
Khwajapasha <Khwajapasha141@gamil.com>
2020-09-18 20:27:43 +0530

    Made Home Page

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Home =()=> {
+        return(
+            <div>
+                
+                <h1 > Welcome To Home </h1>  
+                <p >
+                    Hello, 
+                    This page is made up by Mr. Khwaja  and he is very passionate to learn new things.
+                </p>
+                
+                
+            </div>
+        )
+    
+}
